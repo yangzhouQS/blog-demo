43abc369fdf4c4d65cc9158122bc4d774638cb8b
yangzhou <2638497222@qq.com>
2021-02-28 20:33:44 +0800

    fix: 框架包

---
+++ 框架源码/q.js
@@ -0,0 +1,42 @@
+const items = [
+	{
+		"materialId": 591154085111367,
+		"materialCode": "00008",
+		"materialName": "普通硅酸盐水泥",
+		"materialModel": "P·O 42.5散装",
+		"materialUnit": "吨",
+		"planAmn": 348.285,
+		"factAmnt": 348.856
+	}, {
+		"materialId": 591154085111362,
+		"materialCode": "00003",
+		"materialName": "普通硅酸盐水泥",
+		"materialModel": "P·O 52.5袋装",
+		"materialUnit": "吨",
+		"planAmn": 329.652,
+		"factAmnt": 330.468
+	}, {
+		"materialId": 591154085111367,
+		"materialCode": "00008",
+		"materialName": "普通硅酸盐水泥",
+		"materialModel": "P·O 42.5散装",
+		"materialUnit": "吨",
+		"planAmn": 6.66,
+		"factAmnt": 6.66
+	}, {
+		"materialId": 591154085111362,
+		"materialCode": "00003",
+		"materialName": "普通硅酸盐水泥",
+		"materialModel": "P·O 52.5袋装",
+		"materialUnit": "吨",
+		"planAmn": 66.6,
+		"factAmnt": 66.6
+	}, {
+		"materialId": 591154085111251,
+		"materialCode": "00005",
+		"materialName": "碎石",
+		"materialModel": "0.5-1",
+		"materialUnit": "吨",
+		"planAmn": 477.938,
+		"factAmnt": 479.074
+	}]
